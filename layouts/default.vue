44fcd409838c545a0b6e1660cdcecdc23b37480c
N-eeraj <neeraj7rajeena@gmail.com>
2023-11-15 22:53:34 +0530

    Initial Commit Project setup

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    Nav Bar
+  </nav>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    Footer
+  </footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { options } = useRouter()
+console.log(options.routes)
+
+definePageMeta({
+  layout: 'blank',
+})
+</script>
+
+<template>
+  <div>
+    <h1>
+      Nuxt
+    </h1>
+    <h2>
+      The Intuitive Vue Framework
+    </h2>
+    <p>
+      Nuxt is an open source framework that makes web development intuitive and powerful.
+      Create performant and production-grade full-stack web apps and websites with confidence.
+    </p>
+    <div>
+      <button>
+        Get Started
+      </button>
+      <button>
+        What is Nuxt?
+      </button>
+      <button>
+        Explore Packages
+      </button>
+    </div>
+  </div>
+</template>
