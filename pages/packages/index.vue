a64dc7ead91399461f08db3959d41caa7f435ac9
N-eeraj <neeraj7rajeena@gmail.com>
2023-12-06 09:30:59 +0530

    Basic category listing

--- pages/packages/index.vue
+++ pages/packages/index.vue
@@ -10,6 +10,12 @@ const packages = computed(() => {
   return allPackages
 })
 
+const categories = computed(() => Array.from(new Set(packages.value.map(({ category }) => category))))
+
+const handleCategorySelect = category => {
+  console.log(category)
+}
+
 definePageMeta({
   name: 'All Packages',
   icon: 'ph:package-duotone',
@@ -17,13 +23,29 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col gap-3">
     <h1 class="text-white text-3xl font-medium">
       Nuxt Packages
     </h1>
-    <div class="grid md:grid-cols-2">
-      <div v-for="i in packages">
-        {{ i }}
+
+    <div class="flex gap-x-2">
+      <div class="flex-shrink-0 max-sm:hidden w-72">
+        <span class="text-white">
+          Categories
+        </span>
+        <ul class="mt-3">
+          <li v-for="category in categories" :key="category">
+            <button class="w-full py-1 pl-3 text-light-extra text-start border-l border-l-light-extra/20" @click="handleCategorySelect(category)">
+              {{ category }}
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <div class="grid md:grid-cols-3 lg:grid-cols-4">
+        <div v-for="i in packages">
+          {{ i }}
+        </div>
       </div>
     </div>
   </div>
