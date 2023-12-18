00c488edc90ceeae3b458861545b9b2061d3e18f
N-eeraj <neeraj7rajeena@gmail.com>
2023-12-19 00:08:26 +0530

    Clear search field & prefill from route query

--- components/packages/Search.vue
+++ components/packages/Search.vue
@@ -4,19 +4,26 @@ const { replace } = useRouter()
 
 const searchInput = ref(null)
 
+const setQuery = () => replace({
+  query: {
+    ...route.query,
+    q: searchInput.value.value,
+  },
+  ...route
+})
+
 const handleSearchChange = (event) => {
   if (event.key === '/') {
     if (searchInput.value !== document.activeElement)
       event.preventDefault()
     searchInput.value.focus()
   }
-  replace({
-    query: {
-      ...route.query,
-      q: searchInput.value.value,
-    } ,
-    ...route
-  })
+  setQuery()
+}
+
+const clearSearch = () => {
+  searchInput.value.value = null
+  setQuery()
 }
 
 onMounted(() => {
@@ -31,7 +38,12 @@ onBeforeUnmount(() => {
 <template>
   <div class="relative w-full sm:w-3/5 max-w-66 sm:max-w-56">
     <Icon name="ph:magnifying-glass-duotone" size="20" class="absolute position-center-y left-3 text-light-extra" />
-    <input ref="searchInput" placeholder="Search..." class="w-full pl-10 py-2 bg-gray-900 text-white placeholder:text-light-extra/30 text-sm border border-light-extra/30 focus:border-primary rounded-md focus:rounded-md focus:outline-none" @input="handleSearchChange" />
-    <kbd class="absolute position-center-y right-3 flex-center w-5 aspect-square bg-gray-800 text-light text-xs rounded border border-light-extra/30">/</kbd>
+    <input ref="searchInput" :value="route.query?.q" placeholder="Search..." class="w-full pl-10 py-2 bg-gray-900 text-white placeholder:text-light-extra/30 text-sm border border-light-extra/30 focus:border-primary rounded-md focus:rounded-md focus:outline-none" @input="handleSearchChange" />
+    <div class="absolute position-center-y right-3">
+      <button v-if="route.query?.q" @click="clearSearch">
+        <Icon name="ph:x" size="16" class="text-light-extra" />
+      </button>
+      <kbd v-else class="flex-center w-5 aspect-square bg-gray-800 text-light text-xs rounded border border-light-extra/30">/</kbd>
+    </div>
   </div>
 </template>
