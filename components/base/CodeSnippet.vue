b2b824255ceb22c911d6e422d3e96d52e65b37f4
N-eeraj <neeraj7rajeena@gmail.com>
2024-01-13 15:14:42 +0530

    Implementation of simple counter using Pinia store Added filename in code snippet component

--- components/base/CodeSnippet.vue
+++ components/base/CodeSnippet.vue
@@ -1,18 +1,28 @@
 <script setup>
+defineProps({
+  fileName: {
+    type: String,
+    required: false,
+  },
+})
+
 const code = ref(null)
 const copyCode = () => navigator.clipboard.writeText(code.value.innerText)
 </script>
 
 <template>
-  <section class="flex flex-col gap-y-2 w-full">
+  <section class="flex flex-col gap-y-2">
     <h2>
       <slot name="title" />
     </h2>
     <p class="text-light-extra">
       <slot name="description" />
     </p>
-    <div class="relative min-h-12 bg-bluegray-800 border border-light-extra/25 p-3 rounded-md">
-      <code ref="code" class="text-light">
+    <div class="relative flex flex-col bg-bluegray-800 border border-light-extra/25 rounded-md overflow-hidden">
+      <code v-if="fileName" class="w-full p-3 bg-background-grey text-light text-sm">
+        {{ fileName }}
+      </code>
+      <code ref="code" class="block min-h-12 p-3 text-light text-sm">
         <slot />
       </code>
       <Icon name="ph:copy" class="absolute top-4 right-4 text-light cursor-pointer" @click="copyCode" />
--- pages/packages/pinia.vue
+++ pages/packages/pinia.vue
@@ -1,4 +1,9 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+const demoStore = useDemoStore()
+const { counter } = storeToRefs(demoStore)
+const { updateCount } = demoStore
+
 definePageMeta({
   name: 'Pinia',
   logo: 'pinia.svg',
@@ -10,20 +15,49 @@ definePageMeta({
 <template>
   <PackagesLayout>
     <template #installation>
-      <pre>
-<span class="text-[#FFCB6B]">bun</span> <span class="text-[#C3E88D]">i @pinia/nuxt</span></pre>
+<pre class="overflow-auto">bun i @pinia/nuxt</pre>
     </template>
     <template #usage-description>
-      Add the following to nuxt.config.js
+      Add the following line to nuxt.config.js
     </template>
 
     <template #usage>
-      <pre>
-<span class="text-[#89DDFF]">modules:</span> [<span class="text-[#C3E88D]">'@pinia/nuxt'</span>]</pre>
+<pre class="overflow-auto">modules: ['@pinia/nuxt']</pre>
     </template>
 
     <section>
-      
+      <NuxtLink to="#usage" class="text-white text-2xl font-bold">
+        <h2>
+          Demo
+        </h2>
+      </NuxtLink>
+
+      <div class="flex gap-x-5 flex-wrap">
+        <BaseCodeSnippet file-name="store.js" class="flex-1">
+<pre class="overflow-auto">import { defineStore } from 'pinia'
+
+export const useDemoStore = defineStore('demo', () => {
+  const counter = ref(0)
+  const updateCount = () => counter.value++
+
+  return { counter, updateCount }
+})
+</pre>
+      </BaseCodeSnippet>
+      <BaseCodeSnippet file-name="Counter.vue" class="flex-1">
+<pre class="overflow-auto">
+import { storeToRefs } from 'pinia'
+const demoStore = useDemoStore()
+const { counter } = storeToRefs(demoStore)
+const { updateCount } = demoStore
+</pre>
+        </BaseCodeSnippet>
+      </div>
+
+      <button class="min-w-[180px] mt-5 p-2 bg-primary hover:bg-primary/75 text-background rounded" @click="updateCount">
+        Count is:
+        {{ counter }}
+      </button>
     </section>
   </PackagesLayout>
 </template>
