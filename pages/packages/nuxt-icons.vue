8d2c04c5e86626a0e1745a1038c40462e4fe1628
N-eeraj <neeraj7rajeena@gmail.com>
2024-01-26 19:03:46 +0530

    Basic nuxt icons page setup

---
+++ pages/packages/nuxt-icons.vue
@@ -0,0 +1,48 @@
+<script setup>
+const usageSteps = [
+  'Create a icons folder in assets: assets/icons',
+  'Drop your icons with the .svg extension into the icons folder',
+  'In the project, use <nuxt-icon name="" />, where name is the name of your svg icon from the folder',
+]
+
+definePageMeta({
+  name: 'Nuxt Icons',
+  logo: 'nuxt-icons.avif',
+  category: 'UI',
+  description: 'Use your own automatically imported SVG icons',
+})
+</script>
+
+<template>
+  <PackagesLayout>
+    <template #installation>
+      <pre class="overflow-auto">bun i nuxt-icons</pre>
+    </template>
+
+    <template #setup-description>
+      Add the following line to the modules section of nuxt.config.{ts,js}
+    </template>
+    <template #setup>
+      <pre class="overflow-auto">modules: ['nuxt-icons']</pre>
+    </template>
+
+    <section class="mb-5">
+      <NuxtLink to="#usage" class="package-subtitle">
+        Usage
+      </NuxtLink>
+      <ol class="my-4 text-light">
+        <li v-for="(step, index) of usageSteps" :key="index" class="flex gap-x-2">
+          <span class="text-light-extra">
+            {{ index + 1 }}.
+          </span>
+          {{ step }}
+        </li>
+      </ol>
+
+      <p class="text-light">
+        If you need to use the original color from the svg file (for example, if your icon has defs) you need to use the filled attribute: &lt;nuxt-icon name="mySuperIcon" filled /&gt;
+      </p>
+    </section>
+
+  </PackagesLayout>
+</template>
