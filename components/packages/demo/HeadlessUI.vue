a8de46a0f3ba94cc45a7e770a9b79546c23ef63d
N-eeraj <neeraj7rajeena@gmail.com>
2024-01-28 17:48:46 +0530

    Headleaa UI integrations

---
+++ components/packages/demo/HeadlessUI.vue
@@ -0,0 +1,37 @@
+<script setup>
+const enabled = ref(false)
+</script>
+
+<template>
+  <section>
+    <NuxtLink to="#demo" class="package-subtitle">
+      <h2>
+        Demo
+      </h2>
+    </NuxtLink>
+
+    <BaseCodeSnippet file-name="store.js" class="mb-4">
+      <pre class="overflow-auto">
+&lt;script setup&gt;
+const enabled = ref(false)
+&lt;/script&gt;
+
+&lt;template&gt;
+  &lt;HeadlessSwitch v-model="enabled" :class="enabled ? 'bg-gradient-to-r from-[#62D1F5] to-[#6E6CF6]' : 'bg-white/25'" class="relative inline-flex h-7 w-14 items-center rounded-full"&gt;
+    &lt;span :class="enabled ? 'translate-x-8' : 'translate-x-1'" class="inline-block w-5 aspect-square transform rounded-full bg-white duration-400 ease-in-out" /&gt;
+  &lt;/HeadlessSwitch&gt;
+&lt;/template&gt;</pre>
+    </BaseCodeSnippet>
+
+    <HeadlessSwitch v-model="enabled" :class="enabled ? 'bg-gradient-to-r from-[#62D1F5] to-[#6E6CF6]' : 'bg-white/25'" class="relative inline-flex h-7 w-14 items-center rounded-full">
+      <span :class="enabled ? 'translate-x-8' : 'translate-x-1'" class="inline-block w-5 aspect-square transform rounded-full bg-white duration-400 ease-in-out" />
+    </HeadlessSwitch>
+
+    <p class="text-light mt-4">
+      Checkout more Headless UI components at the official site
+      <NuxtLink to="https://headlessui.com/" target="_blank" rel="nofollow" class="text-primary">
+        https://headlessui.com/
+      </NuxtLink>
+    </p>
+  </section>
+</template>
---
+++ pages/packages/headless-ui.vue
@@ -0,0 +1,25 @@
+<script setup>
+definePageMeta({
+  name: 'Headless UI',
+  logo: 'headlessui.avif',
+  category: 'UI',
+  description: 'Headless UI integration for Nuxt. Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind CSS',
+})
+</script>
+
+<template>
+  <PackagesLayout>
+    <template #installation>
+      <pre class="overflow-auto">bun i -D nuxt-headlessui</pre>
+    </template>
+
+    <template #setup-description>
+      Add the following line to the modules section of nuxt.config.{ts,js}
+    </template>
+    <template #setup>
+      <pre class="overflow-auto">modules: ['nuxt-headlessui']</pre>
+    </template>
+
+    <PackagesDemoHeadlessUI />
+  </PackagesLayout>
+</template>
