c87910608e65f8c2a993ee36aabb40a563a88736
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-02 13:00:40 +0530

    UnoCSS integration

--- pages/packages/uno-css.vue
+++ pages/packages/uno-css.vue
@@ -1,25 +1,25 @@
 <script setup>
 definePageMeta({
-  name: 'Tailwind CSS',
-  logo: 'tailwindcss.svg',
+  name: 'UnoCSS',
+  logo: 'unocss.svg',
   category: 'UI',
-  description: 'Add Tailwind CSS to your Nuxt application in seconds with PurgeCSS included for minimal CSS',
+  description: 'The instant on-demand atomic CSS engine',
 })
 </script>
 
 <template>
   <PackagesLayout>
     <template #installation>
-      <pre class="overflow-auto">bun i @nuxtjs/tailwindcss</pre>
+      <pre class="overflow-auto">bun i @unocss/nuxt</pre>
     </template>
 
     <template #setup-description>
       Add the following line to the modules section of nuxt.config.{ts,js}
     </template>
     <template #setup>
-      <pre class="overflow-auto">modules: ['@nuxtjs/tailwindcss']</pre>
+      <pre class="overflow-auto">modules: ['@unocss/nuxt']</pre>
     </template>
 
-    <PackagesDemoTailwind />
+    <PackagesDemoUnoCSS />
   </PackagesLayout>
 </template>
