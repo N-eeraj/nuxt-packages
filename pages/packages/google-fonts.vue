b64c0efa23a8839110d1614b6cd3e08bbf8de0fe
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-06 21:13:40 +0530

    google-fonts integration & basic demo

---
+++ pages/packages/google-fonts.vue
@@ -0,0 +1,35 @@
+<script setup>
+definePageMeta({
+  name: 'Google Fonts',
+  logo: 'google-fonts.avif',
+  category: 'Fonts',
+  description: 'Google Fonts module for Nuxt',
+})
+</script>
+
+<template>
+  <PackagesLayout>
+    <template #installation>
+      <pre class="overflow-auto">bun i -D @nuxtjs/google-fonts</pre>
+    </template>
+
+    <template #setup-description>
+      Add the following lines to nuxt.config.{ts,js}
+    </template>
+    <template #setup>
+      <pre class="overflow-auto">modules: ['@nuxtjs/google-fonts'],
+googleFonts: {
+  families: {
+    Montserrat: true,
+    Lato: [100, 900],
+    'Protest Revolution': {
+      wght: [100, 400],
+      ital: [100]
+    },
+  }
+},</pre>
+    </template>
+
+    <PackagesDemoGoogleFonts />
+  </PackagesLayout>
+</template>
