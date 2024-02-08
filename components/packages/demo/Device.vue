17aee7d8bba7866d21475a64a3a96dc97163fa1b
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-08 21:41:51 +0530

    device module integration & fallback icons for package logos

---
+++ components/packages/demo/Device.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <NuxtLink to="#demo" class="package-subtitle">
+      <h2>
+        Demo
+      </h2>
+    </NuxtLink>
+
+    <BaseCodeSnippet file-name="Device.vue" class="mb-4">
+      <pre class="overflow-auto">&lt;template&gt;
+  &lt;div"&gt;
+    &lt;template v-if="$device.isDesktop"&gt;
+      Desktop
+    &lt;/template&gt;
+    &lt;template v-else-if="$device.isTablet"&gt;
+      Tablet
+    &lt;/template&gt;
+    &lt;template v-else&gt;
+      Mobile
+    &lt;/template&gt;
+  &lt;/div&gt;
+&lt;/template&gt;</pre>
+    </BaseCodeSnippet>
+
+    <div class="text-white">
+      <template v-if="$device.isDesktop">
+        Desktop
+      </template>
+      <template v-else-if="$device.isTablet">
+        Tablet
+      </template>
+      <template v-else>
+        Mobile
+      </template>
+    </div>
+  </section>
+</template>
---
+++ pages/packages/device.vue
@@ -0,0 +1,24 @@
+<script setup>
+definePageMeta({
+  name: 'Devices',
+  category: 'Extensions',
+  description: 'Device detection module for Nuxt',
+})
+</script>
+
+<template>
+  <PackagesLayout>
+    <template #installation>
+      <pre class="overflow-auto">bun i -D @nuxtjs/device</pre>
+    </template>
+
+    <template #setup-description>
+      Add the following line to the modules section of nuxt.config.{ts,js}
+    </template>
+    <template #setup>
+      <pre class="overflow-auto">modules: ['@nuxtjs/device']</pre>
+    </template>
+
+    <PackagesDemoDevice />
+  </PackagesLayout>
+</template>
