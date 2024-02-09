9a0867ad3cead7c413394337eb5652b43a13bafe
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-09 21:03:18 +0530

    color-mode integration & basic demo

---
+++ components/packages/demo/ColorMode.vue
@@ -0,0 +1,79 @@
+<script setup>
+const colorModes = ref([
+  'System',
+  'Light',
+  'Dark',
+  'Sepia',
+])
+</script>
+
+<template>
+  <section>
+    <NuxtLink to="#demo" class="package-subtitle">
+      <h2>
+        Demo
+      </h2>
+    </NuxtLink>
+
+    <BaseCodeSnippet file-name="Fonts.vue" class="mb-4">
+      <pre class="overflow-auto">&lt;template&gt;
+  &lt;h1 class="demo-text"&gt;
+    Color mode: &#123;&#123; $colorMode.value &#125;&#125;
+  &lt;/h1&gt;
+  &lt;select v-model="$colorMode.preference" class="demo-select"&gt;
+    &lt;option value="system"&gt;
+      System
+    &lt;/option&gt;
+    &lt;option value="light"&gt;
+      Light
+    &lt;/option&gt;
+    &lt;option value="dark"&gt;
+      Dark
+    &lt;/option&gt;
+    &lt;option value="sepia"&gt;
+      Sepia
+    &lt;/option&gt;
+  &lt;/select&gt;
+&lt;/template&gt;
+
+&lt;style scoped&gt;
+.dark-mode .demo-text, .dark-mode .demo-select {
+  background-color: black;
+  color: white;
+}
+.light-mode .demo-text, .dark-mode .demo-select {
+  background-color: white;
+  color: black;
+}
+.sepia-mode .demo-text, .sepia-mode .demo-select {
+  background-color: #FED;
+  color: #300;
+}
+&lt;/style&gt;</pre>
+    </BaseCodeSnippet>
+
+    <h1 class="demo-text p-2 rounded">
+      Color mode: {{ $colorMode.value }}
+    </h1>
+    <select v-model="$colorMode.preference" class="demo-select mt-4 p-1">
+      <option v-for="mode of colorModes" :value="mode.toLocaleLowerCase()" :key="mode">
+        {{ mode }}
+      </option>
+    </select>
+  </section>
+</template>
+
+<style scoped>
+.dark-mode .demo-text, .dark-mode .demo-select {
+  background-color: black !important;
+  color: white !important;
+}
+.light-mode .demo-text, .dark-mode .demo-select {
+  background-color: white;
+  color: black;
+}
+.sepia-mode .demo-text, .sepia-mode .demo-select {
+  background-color: #FED;
+  color: #300;
+}
+</style>
---
+++ pages/packages/color-mode.vue
@@ -0,0 +1,25 @@
+<script setup>
+definePageMeta({
+  name: 'Color Mode',
+  logo: 'color-mode.avif',
+  category: 'UI',
+  description: 'Dark and Light mode for Nuxt with auto detection',
+})
+</script>
+
+<template>
+  <PackagesLayout>
+    <template #installation>
+      <pre class="overflow-auto">bun i -D @nuxtjs/color-mode</pre>
+    </template>
+
+    <template #setup-description>
+      Add the following lines to nuxt.config.{ts,js}
+    </template>
+    <template #setup>
+      <pre class="overflow-auto">modules: ['@nuxtjs/color-mode']</pre>
+    </template>
+
+    <PackagesDemoColorMode />
+  </PackagesLayout>
+</template>
