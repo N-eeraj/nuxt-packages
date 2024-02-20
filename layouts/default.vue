12d4d6e5fc15c437c17f57aeac7ec02b0e189ed8
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-20 20:51:59 +0530

    Basic useClipboard demo

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@ const openSideBar = ref(false)
   <LayoutNavBar v-model:side-bar="openSideBar" />
   <main class="relative full-screen md:flex gap-x-4 pl-screen pt-20 pb-10 bg-background overflow-y-auto">
     <LayoutSideBar v-model:open="openSideBar" />
-    <section class="w-full pr-screen overflow-x-hidden">
+    <section class="w-full p-1 pr-screen overflow-x-hidden">
       <slot />
     </section>
   </main>
---
+++ pages/packages/vueuse/state/useClipboard.vue
@@ -0,0 +1,66 @@
+<script setup>
+const source = ref('Hello')
+const { text, copy, copied, isSupported } = useClipboard({ source })
+
+definePageMeta({
+  name: 'useClipboard',
+  category: 'Browser',
+})
+</script>
+
+<template>
+  <h1 class="text-white text-2xl">
+    useClipboard    
+  </h1>
+
+  <div class="flex gap-x-5 flex-wrap">
+    <BaseCodeSnippet file-name="store.js" class="flex-1 w-full md:w-5/12">
+      <pre class="overflow-auto">import { createGlobalState, useStorage } from '@vueuse/core'
+
+export const useVueUseStore = createGlobalState(() => useStorage('vueuse-local-storage-counter', 0))</pre>
+    </BaseCodeSnippet>
+
+    <BaseCodeSnippet file-name="Counter.vue" class="flex-1 w-full md:w-5/12">
+      <pre class="overflow-auto">&lt;script setup&gt;
+  const source = ref('Hello')
+  const { text, copy, copied, isSupported } = useClipboard({ source })
+&lt;/script&gt;
+
+&lt;template&gt;
+  &lt;div v-if="isSupported"&gt;
+    &lt;input v-model="source" type="text" /&gt;
+    &lt;p &gt;
+      Current copied:
+      &lt;code&gt;
+        {{ text || 'none' }}
+      &lt;/code&gt;
+    &lt;/p&gt;
+    &lt;button @click="copy(source)"&gt;
+      &lt;span v-if="!copied">Copy&lt;/span&gt;
+      &lt;span v-else>Copied!&lt;/span&gt;
+    &lt;/button&gt;
+  &lt;/div&gt;
+  &lt;p v-else&gt;
+    Your browser does not support Clipboard API
+  &lt;/p&gt;
+&lt;/template&gt;</pre>
+    </BaseCodeSnippet>
+  </div>
+
+  <div v-if="isSupported" class="mt-4 flex flex-col gap-y-2">
+    <input v-model="source" type="text" class="p-2 bg-background-grey text-light-extra" />
+    <p class=" text-white-light">
+      Current copied:
+      <code>
+        {{ text || 'none' }}
+      </code>
+    </p>
+    <button class="w-32 p-2 bg-primary hover:bg-primary/75 text-background rounded" @click="copy(source)">
+      <span v-if="!copied">Copy</span>
+      <span v-else>Copied!</span>
+    </button>
+  </div>
+  <p v-else class="text-light">
+    Your browser does not support Clipboard API
+  </p>
+</template>
