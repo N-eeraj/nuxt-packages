48cd3755ad4c15ffde50acba6bb32a9f7dee66ca
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-24 22:33:30 +0530

    vueuse get utility demo

---
+++ pages/packages/vueuse/utilities/get.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { get } from '@vueuse/core'
+const refVal = ref('test')
+console.log(get(refVal))
+
+definePageMeta({
+  name: 'get',
+  category: 'Utilities',
+})
+</script>
+
+<template>
+  <h1 class="text-white text-2xl">
+    get    
+  </h1>
+
+  <BaseCodeSnippet file-name="get.vue" class="w-fit">
+    <pre class="overflow-auto">&lt;script setup&gt;
+  import { get } from '@vueuse/core'
+  const refVal = ref('test')
+  console.log(get(refVal))
+&lt;/script&gt;</pre>
+  </BaseCodeSnippet>
+</template>
