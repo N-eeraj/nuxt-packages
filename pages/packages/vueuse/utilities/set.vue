21189afe9b1a514b9033d2845e92fce719759d0f
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-25 20:11:21 +0530

    vueuse set utility demo

---
+++ pages/packages/vueuse/utilities/set.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { set } from '@vueuse/core'
+const refVal = ref('')
+console.log(refVal.value)
+set(refVal, 'test')
+console.log(refVal.value)
+
+definePageMeta({
+  name: 'set',
+  category: 'Utilities',
+})
+</script>
+
+<template>
+  <h1 class="text-white text-2xl">
+    set    
+  </h1>
+
+  <BaseCodeSnippet file-name="set.vue" class="w-fit">
+    <pre class="overflow-auto">&lt;script setup&gt;
+  import { set } from '@vueuse/core'
+  const refVal = ref('')
+  console.log(refVal)
+  set(refVal, 'test')
+  console.log(refVal)
+&lt;/script&gt;</pre>
+  </BaseCodeSnippet>
+</template>
