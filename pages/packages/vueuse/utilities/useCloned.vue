11122274f2e5e7480c2421d1b603bb78e55d2c20
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-27 11:59:44 +0530

    vueuse useCloned utility demo

---
+++ pages/packages/vueuse/utilities/useCloned.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useCloned } from '@vueuse/core'
+const original = ref({ key: 'value' })
+const { cloned } = useCloned(original)
+original.value.key = 'some new value'
+console.log(original.value.key)
+console.log(cloned.value.key)
+
+definePageMeta({
+  name: 'useCloned',
+  category: 'Utilities',
+})
+</script>
+
+<template>
+  <h1 class="text-white text-2xl">
+    useCloned
+  </h1>
+
+  <BaseCodeSnippet file-name="clone.vue" class="w-fit">
+    <pre class="overflow-auto">&lt;script setup&gt;
+  import { useCloned } from '@vueuse/core'
+  const original = ref({ key: 'value' })
+  const { cloned } = useCloned(original)
+  original.value.key = 'some new value'
+  console.log(original.value.key)
+  console.log(cloned.value.key)
+&lt;/script&gt;</pre>
+  </BaseCodeSnippet>
+</template>
