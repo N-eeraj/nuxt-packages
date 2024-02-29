8d299e93c5ebaabc8fb74d3f069d8dc9de0263d5
N-eeraj <neeraj7rajeena@gmail.com>
2024-02-29 22:10:08 +0530

    Basic shiki setup & usage on auto-animate demo page

--- composables/data.ts
+++ composables/data.ts
@@ -1,7 +1,15 @@
 import techStack from '@/data/tech-stack.json'
 import packageCategoriesIcons from '@/data/package-categories-icons.json'
+import autoAnimateDemo from '@/data/demo/auto-animate.txt?raw'
+
+const useDemoCode = () => {
+  return {
+    autoAnimateDemo,
+  }
+}
 
 export {
   techStack,
   packageCategoriesIcons,
+  useDemoCode,
 }
