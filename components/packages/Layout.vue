9aa7d37dd90a6472982d3a638c4cf1b1d3bd7e81
N-eeraj <neeraj7rajeena@gmail.com>
2024-03-04 18:55:19 +0530

    Minor refactoring

--- components/packages/Layout.vue
+++ components/packages/Layout.vue
@@ -13,17 +13,18 @@ defineProps({
 })
 const { meta } = useRoute()
 
+const pageImage = `/images/logos/${meta.logo}`
 useMeta({
   pageTitle: `${meta.name} | Nuxt Packages`,
   pageDescription: meta.description,
-  pageImage: `/images/logos/${meta.logo}`,
+  pageImage,
 })
 </script>
 
 <template>
   <div class="flex flex-col gap-y-2 h-30">
     <div class="flex items-center gap-x-4">
-      <img v-if="meta.logo" :src="`/images/logos/${meta.logo}`" :alt="meta.name" class="w-12" />
+      <img v-if="meta.logo" :src="pageImage" :alt="meta.name" class="w-12" />
       <Icon v-else name="ph:puzzle-piece-duotone" class="box-content w-6 h-6 p-3 bg-gray-800 text-gray-400 rounded-lg" />
       <h1 class="text-white text-3xl font-bold">
         {{ meta.name }}
