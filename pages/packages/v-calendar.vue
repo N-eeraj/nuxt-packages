cc633d83cb9c28e52cbdd124d2a690d033d82a8c
N-eeraj <neeraj7rajeena@gmail.com>
2024-04-25 18:37:44 +0530

    Basic VCalendar integration

---
+++ pages/packages/v-calendar.vue
@@ -0,0 +1,20 @@
+<script setup>
+const installationCode = `npm i -D @samk-dev/nuxt-vcalendar`
+const setUpCode = `modules: ['@samk-dev/nuxt-vcalendar']`
+
+definePageMeta({
+  name: 'VCalendar',
+  logo: 'vcalendar.avif',
+  category: 'Libraries',
+  description: 'VCalendar module for Nuxt',
+})
+</script>
+
+<template>
+  <PackagesLayout :installation-code :set-up-code>
+    <template #setup-description>
+      Add the following line to the modules section of nuxt.config.{ts,js}
+    </template>
+    <PackagesDemoVCalendar />
+  </PackagesLayout>
+</template>
