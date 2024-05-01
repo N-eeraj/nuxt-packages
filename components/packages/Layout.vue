44f56bb397ca0528cb302a30602ea66a983e2bb7
N-eeraj <neeraj7rajeena@gmail.com>
2024-05-01 19:25:22 +0530

    Added package layout breadcrumb

--- components/packages/Layout.vue
+++ components/packages/Layout.vue
@@ -14,6 +14,9 @@ defineProps({
 const { meta } = useRoute()
 
 const pageImage = `/images/logos/${meta.logo}`
+
+const categoryIcon = packageCategoriesIcons[meta.category]
+
 useMeta({
   pageTitle: `${meta.name} | Nuxt Packages`,
   pageDescription: meta.description,
@@ -22,10 +25,27 @@ useMeta({
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-2 h-30">
+  <div class="flex items-center gap-x-2 text-light-extra">
+    <NuxtLink to="/packages" class="flex items-center gap-x-1">
+      <Icon :name="categoryIcon" size="18" />
+      <span>
+        Modules
+      </span>
+    </NuxtLink>
+    <Icon name="ph:caret-right" />
+    <NuxtLink :to="`/packages?category=${meta.category}`">
+      {{ meta.category }}
+    </NuxtLink>
+    <Icon name="ph:caret-right" />
+    <span class="text-primary">
+      {{ meta.name }}
+    </span>
+  </div>
+
+  <div class="flex flex-col gap-y-2 h-30 mt-8">
     <div class="flex items-center gap-x-4">
       <img v-if="meta.logo" :src="pageImage" :alt="meta.name" class="w-12" />
-      <Icon v-else :name="packageCategoriesIcons[meta.category]"  class="box-content w-6 h-6 p-3 bg-gray-800 text-gray-400 rounded-lg" />
+      <Icon v-else :name="categoryIcon" class="box-content w-6 h-6 p-3 bg-gray-800 text-gray-400 rounded-lg" />
       <h1 class="text-white text-3xl font-bold">
         {{ meta.name }}
       </h1>
