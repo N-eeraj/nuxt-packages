367cb2937302d8dfa45eed021b68a16affd42fae
N-eeraj <neeraj7rajeena@gmail.com>
2024-05-03 11:22:44 +0530

    nuxt aos integration

---
+++ pages/packages/aos.vue
@@ -0,0 +1,17 @@
+<script setup>
+const installationCode = `npm i -D nuxt-aos`
+const setUpCode = `modules: ['nuxt-aos']`
+
+definePageMeta({
+  name: 'AOS',
+  logo: 'aos.avif',
+  category: 'Libraries',
+  description: 'A Nuxt module that adds Animate On Scroll',
+})
+</script>
+
+<template>
+  <PackagesLayout :installation-code :set-up-code>
+    <PackagesDemoAOS />
+  </PackagesLayout>
+</template>
