11a3fddc35e0b738c249d321fb0a9c3349888b99
N-eeraj <neeraj7rajeena@gmail.com>
2024-07-14 12:44:52 +0530

    Minor UI fix

--- components/layout/SideBar.vue
+++ components/layout/SideBar.vue
@@ -11,25 +11,19 @@ const route = useRoute()
 
 const mainNavigations = computed(() =>
   options.routes.filter(({ path }) => [ '/', '/about', '/packages' ].includes(path))
-    .sort(({ path: firstPath }, { path: secondPath }) => {
-      if (firstPath < secondPath) return -1
-      return 1
-    })
+    .sort(({ path: firstPath }, { path: secondPath }) => firstPath < secondPath ? -1 : 1)
 )
 
 const packagesNavigations = computed(() =>
   options.routes.filter(({ path }) => path.startsWith('/packages/') && path.split('/').length === 3)
-    .sort(({ path: firstPath }, { path: secondPath }) => {
-      if (firstPath < secondPath) return -1
-      return 1
-    })
+    .sort(({ path: firstPath }, { path: secondPath }) => firstPath < secondPath ? -1 : 1)
 )
 
 const currentPath = computed(() => route.path)
 </script>
 
 <template>
-  <aside class="fixed md:sticky top-0 left-0 flex-shrink-0 w-screen md:w-56 h-full max-sm:px-4 max-sm:pt-20 pb-6 bg-background overflow-y-auto z-1" :class="{'max-md:hidden': !open}">
+  <aside class="fixed md:sticky top-0 left-0 flex-shrink-0 w-screen md:w-56 h-full max-sm:px-4 max-sm:pt-20 pb-6 bg-background overflow-y-auto z-50" :class="{'max-md:hidden': !open}">
     <ul class="flex flex-col gap-y-2.5 p-1 text-light/80 text-sm">
       <li v-for="({ name, path, meta }) in mainNavigations" :key="path">
         <NuxtLink :to="path" class="flex items-center gap-x-2 w-full cursor-pointer text-xl md:text-lg hover:text-light" :class="{ '!text-primary': currentPath === path }" @click="$emit('update:open', false)">
